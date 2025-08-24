e8f91205d84100d7ccd78cb70182b491e12c70d2
AKANSHUGARG <gargakanshu29@gmail.com>
2025-08-24 20:22:02 +0530

    working on project

---
+++ real-estate/src/routes/homePage/homePage.jsx
@@ -0,0 +1,37 @@
+import SearchBar from '../searchBar/searchBar'
+import './homePage.scss'
+
+function HomePage(){
+    return (
+        <div className='homePage'>
+            <div className="textContainer">
+                <div className="wrapper">
+                    <h1>
+                        Solution For RealEstate's Trade , Find and Get Dream Place
+                    </h1>
+                    <p>lfnjiefn enfefifn dnfieffn nefiren eifnreivn fdfnvdfvreg erg trgr  ger gerg  gfer gfergf  fvgre  g  fjh tgdf rth rfb dfg rh rfef grtrfgvrhgrgr brtgrgr tg rgr grrgregreg  ggrnfn iin inin niini ytv tv yby rrgfefe</p>
+                    <SearchBar/>
+                    <div className="boxes">
+                        <div className="box">
+                            <h1>16+</h1>
+                            <h2>Years of Experience</h2>
+                        </div>
+                        <div className="box">
+                            <h1>200</h1>
+                            <h2>Award Gained</h2>
+                        </div>
+                        <div className="box">
+                            <h1>1200+</h1>
+                            <h2>Propert Ready</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="imgContainer">
+                <img src="bg.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
