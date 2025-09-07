f00091af84f0ab5fb9f21042007b3b0c695c5b26
AKANSHUGARG <gargakanshu29@gmail.com>
2025-09-08 00:50:15 +0530

    starting with backend

---
+++ backend/api/controllers/auth.controller.js
@@ -0,0 +1,30 @@
+import bcrypt, { hash } from "bcrypt";
+import prisma from "../lib/prisma.js";
+
+
+export const register = async (req,res) =>{
+    const {username, email, password} = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    console.log(hashedPassword);
+
+    const newUser = await prisma.user.create({
+        data:{
+            username,
+            email,
+            password:hashedPassword,
+        },
+    });
+
+    console.log(newUser);
+    
+}
+
+export const login = (req,res) =>{
+    //db operations
+}
+
+export const logout = (req,res) =>{
+    //db operations
+}
